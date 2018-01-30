a84fd1ba18e86455b4961bb4452d8524f0c4c85d
sunshine <827600907@qq.com>
2018-01-30 18:36:10 +0800

    elementui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="header">
-          dddddddddddddddddddddd
+    <div class="back_icon">返回</div>
+    <h1>我的目标</h1>
+    <div class="more">
+      <img src="../assets/img/see_head_more.png"/>
+    </div>
   </div>
 </template>
 
@@ -14,10 +18,32 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
   .header{
-    height:rem(44);
+    height:44px;
     width:100%;
     background: #1C1C20;
+    .back_icon{
+      color:#fff;
+      font-size:18px;
+      float:left;
+      line-height:44px;
+      padding-left:10px;
+    }
+    h1{
+      font-size:18px;
+      color:#fff;
+      margin-left:40%;
+      line-height:44px;
+    }
+    .more{
+     img {
+       width: 30px;
+       height: 30px;
+       float: right;
+       margin-top:-35px;
+       padding-right:10px;
+     }
+    }
   }
 </style>
