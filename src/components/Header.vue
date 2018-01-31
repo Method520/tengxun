e35d01966c4c1b0d678fafb097128fa740838118
sunshine <827600907@qq.com>
2018-01-31 09:12:34 +0800

    引入elementui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,19 @@
     <div class="more">
       <img src="../assets/img/see_head_more.png"/>
     </div>
+    <div class="block">
+      <span class="demonstration">有默认值</span>
+      <el-color-picker v-model="color1"></el-color-picker>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
   data () {
     return {
+      color1: '#409EFF'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router' // vue-router
 import store from './store' // vuex
+import ElementUI from 'element-ui'
 
 import '@/common/scss/index.scss'
 
@@ -24,7 +25,7 @@ Vue.use(VueLazyload, {
 })
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
